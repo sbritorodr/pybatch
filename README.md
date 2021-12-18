1e22c1ef46f1317c2db6b6e32dafda2e67359eea
sbritorodr <sbritorodr@gmail.com>
2021-12-18 16:09:50 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# PyBatch
+A easy to use python script that can batch all files inside a folder
+
+For example, with this script you can run ffprobe with all files inside this folder, or run ffmpeg y convert all files into any desired format
+
+## Installation:
+1. Download pybatch.py
+2. Place the file inside your working directory
+
+## Usage
+The program tells step by step how to select the format, the directory and the command
+### Directory selection
+As soon as you execute `pybatch.py`, the script will prompt this:
+
+`Add your folder path or leave it blank to select "./input ":`
+
+The directory must be given as full path or relative path:
+* Full path example: `/usr/user/input/` it must start with "`/`"
+* Relative path example: `./input`it must start on a period
+
+### File extention selection
+You need to select your file type. If you don't type anything it will select all files inside the folder
+
+You should provide the file extension starting with a period:
+
+`.txt`, `.pdf`, `.mp4`...
+
+### Command input
+If you type "h", a help screen will pop out if you don't remember the syntax
+You need to write you batch command using a speciffic syntax. For example:
+
+`ffmpeg -i [i] [o].mp3`
+
+Whether "`[i]`" is the input parameter and the "`[o]`" is the output parameter
+You don't have to add the output parameter if you don't need it in the command, but the input parameter is mandatory. 
+
+For example: `ffprobe -i [i]`
+
+| :exclamation:  | If your files has spaces in the name, it will be replaced with underscores   |
+|---------------| :--------------------------|
+
+
+By default, your files will be stored in ./output
---
+++ pybatch.py
@@ -0,0 +1,86 @@
+# pybatch
+# This program executes any instruction for all files inside a folder 
+# (e.g. convert all these mp4 files into mkv using ffmpeg)
+
+# the idea is to take, for example:
+# ffmpeg -i [i] [o]
+# sustitute input for each instance and generate an output with the same 
+# name inside an ./output folder
+
+import os
+
+# help function
+def help():
+    print(
+    '''
+    This program needs a command, an input [i] and output [o]
+    For example, in order to convert some files in ffmpeg you 
+    need to write this:
+
+    ffmpeg -i [i] [o].mkv
+    ''')
+# get working directoy
+pwd = os.path.dirname(os.path.abspath(__file__))
+
+# get folder:
+input_folder = str(input("Add your folder path or leave it blank to select \"./input/ \": \n")) or "./input/"
+print("You selected this folder: " + input_folder)
+
+directory = pwd
+if input_folder.startswith('.'):
+    input_folder = input_folder.replace('.', '')
+    directory += input_folder
+elif input_folder.startswith('/'):
+    directory = input_folder
+print(directory)
+# select file type:
+file_type = str(input("select file type (.txt, .pdf, .rar...)")) or ""
+print("you selected \"" + file_type + "\"")
+
+# delete all whitespaces
+def remove_whitespaces(parent):
+    for path, folders, files in os.walk(parent):
+        for f in files:
+            os.rename(os.path.join(path, f), os.path.join(path, f.replace(' ', '_')))
+        for i in range(len(folders)):
+            new_name = folders[i].replace(' ', '_')
+            os.rename(os.path.join(path, folders[i]), os.path.join(path, new_name))
+            folders[i] = new_name
+
+remove_whitespaces(directory)
+
+# Loop to get all files
+list_files = []
+for file in os.listdir(directory):
+    if file.endswith(file_type):
+        list_files.append(file)
+print(list_files)
+
+# check if you mess up something
+if list_files == []:
+    raise Exception("No files are selected. Check if you have some files or you input folder exists")
+
+# command string. It substitutes the [i] selection and the [o] with the algorithm
+
+command = str(input("Add your command here. Write h if you need help:\n" )) or "ffmpeg -i [i] [o].mp3"
+command_def = command # save the variable anywhere for the "main" loop
+if command == 'h':
+    help()
+elif "[i]" not in command:
+    raise Exception("you didn't add [i] or [o].")
+if '[o]' in command:
+    try: os.mkdir("output")
+    except: 0
+
+# Command loop with [i] and [o] replacement
+for file in list_files:
+    command = command_def
+    output_file = file.replace(file_type, "") # remove the file extension, to avoid '.mp4.mp3' gibberish
+    command = command.replace('[i]', directory + file) 
+    if "[o]" not in command:
+        command = command.replace('[o]', "")
+    else: 
+        command = command.replace('[o]', pwd + "/output/" + output_file)
+    print(command)
+    os.system(command)
+print("\n Script finished with no errors")
